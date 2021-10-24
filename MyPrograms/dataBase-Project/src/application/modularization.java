ebbedfaebbdfcd67015188e8965c10653231197b
NTTeixeira <64165617+NTTeixeira@users.noreply.github.com>
2021-10-24 20:10:46 -0300

    Add files via upload

---
+++ MyPrograms/dataBase-Project/src/application/modularization.java
@@ -0,0 +1,16 @@
+package application;
+
+import javax.swing.JOptionPane;
+
+public class modularization {
+	
+	public static void connection(String[] args) {
+		
+		
+		String login = JOptionPane.showInputDialog("Login:");
+		String senha = JOptionPane.showInputDialog("Senha:");
+		
+		
+	}
+
+}
---
+++ MyPrograms/dataBase-Project/src/db/DB.java
@@ -0,0 +1,47 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DB {
+
+	private static Connection conn = null;
+	private static Statement st = null;
+
+	public static Connection getConnection() {
+		String servidor = "jdbc:mysql://localhost:3306/bancoteste";
+		String usuario = "root";
+		String senha = "chessus123";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		if (conn == null) {
+			try {
+				Class.forName(driver);
+				conn = DriverManager.getConnection(servidor, usuario, senha);
+				setSt(conn.createStatement());
+			} catch (Exception e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static Statement getSt() {
+		return st;
+	}
+
+	public static void setSt(Statement st) {
+		DB.st = st;
+	}
+}
