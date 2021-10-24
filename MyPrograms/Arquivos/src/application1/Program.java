a05521fbf8530dde6a2006b105438b8970de3818
NTTeixeira <64165617+NTTeixeira@users.noreply.github.com>
2021-10-24 19:53:54 -0300

    Add files via upload

---
+++ MyPrograms/Arquivos/src/application1/Program.java
@@ -0,0 +1,27 @@
+package application1;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] {"1 ", "2", "3"};
+		
+		String path = "C:\\Workspace\\out.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
